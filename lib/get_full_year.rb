7a840facd3100f6a6b7a93b436b80766f189d773
Caius Durling <dev@caius.name>
2025-01-03 11:00:42 +0000

    Add the structure of the gem

---
+++ lib/get_full_year.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative "get_full_year/version"
+
+module GetFullYear
+end
---
+++ lib/get_full_year/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GetFullYear
+  VERSION = "0.1.0"
+end
